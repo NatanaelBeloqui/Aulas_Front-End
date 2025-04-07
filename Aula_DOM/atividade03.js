b9e86e36692f3ad4ffe43281ae228dcd81c54a8c
Natanael <nbdb2005@gmail.com>
2025-04-07 20:39:24 -0300

    Teste

--- Aula_DOM/atividade03.js
+++
@@ -1,9 +0,0 @@
-let paragrafo = document.getElementById('myParagrafo');
-
-paragrafo.addEventListener('mouseover', function() {
-    paragrafo.classList.add('hover');
-});
-
-paragrafo.addEventListener('mouseover', function() {
-    paragrafo.classList.remove('hover');
-});
