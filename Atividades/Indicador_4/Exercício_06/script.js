ff2bd54d9b9e8ffd6cee6280390dc9c58767017a
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-11 01:25:11 -0300

    Exercício 6 concluído

--- Atividades/Indicador_4/Exercício_06/script.js
+++ Atividades/Indicador_4/Exercício_06/script.js
@@ -1,15 +1,21 @@
 function mostrar() {
     const div = document.querySelector('.resultado');
-    const medicamento = document.querySelector('#medicamento').value;
+    const produto = document.querySelector('#produto').value;
     return div.innerHTML = `
-    <h2>Promoção de ${medicamento}</h2>
-    <h2>Leve 2 por apenas R$: ${calcularPromocao()}</h2>
+    <h2>${produto} - Promoção: Leve 3 por R$: ${calcularPromocao()}</h2>
+    <h2>O 3º produto custa apenas R$:${calcularDescontoProduto3()}</h2>
     `;
+}
+
+function calcularDescontoProduto3() {
+    const preco = document.querySelector('#preco').value;
+    const produto3 = preco / 2;
+    return produto3;
 }
 
 function calcularPromocao() {
     const preco = parseFloat(document.querySelector('#preco').value);
-    const promocao = preco * 2;
+    const promocao = (preco * 2) + calcularDescontoProduto3();
 
-    return Math.floor(promocao).toFixed(2);
+    return promocao.toFixed(2);
 }
