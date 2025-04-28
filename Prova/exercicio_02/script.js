edf3c5916921c8d4e30700f17db13a250729f731
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2025-04-28 16:24:28 -0300

    em desenvolvimento

--- Prova/exercicio_02/script.js
+++ Prova/exercicio_02/script.js
@@ -1,41 +1,40 @@
-// vetor que armazena todas as notícias
 const noticias = [];
 
-// atualiza o texto que mostra a contagem
-function atualizarContagem() {
-  document.getElementById('info-contagem')
-    .textContent = `Notícias cadastradas: ${noticias.length}`;
-}
+document.querySelector('#noticias-cadastradas').innerText = 0;
 
-// adiciona uma notícia ao vetor e atualiza a contagem
 function adicionarNoticia() {
-  const campo = document.getElementById('input-noticia');
-  const texto = campo.value.trim();
 
-  if (!texto) {
-    alert('Informe uma notícia válida.');
-    return;
+  const noticiaCadastrada = document.querySelector('#noticia').value;
+
+  if(noticiaCadastrada) {
+    noticias.push(noticiaCadastrada);
+    mostrarNoticiasCadastradas(noticias);
+    document.querySelector('#noticia').value = '';
+  } else {
+    alert('Insira uma notícia');
   }
 
-  noticias.push(texto);
-  campo.value = '';
-  atualizarContagem();
 }
 
-function listarNoticias() {
-if (noticias.length === 0) {
-alert('Não há notícias cadastradas.');
-return;
+function listarNoticias (noticias, qtd) {
+
+  // const divNoticias = document.querySelector('.noticias');
+  const paragrafo = document.createElement('p');
+  const quantidadeNoticias = document.querySelector('.qtd-noticias');
+  const ultimasNoticias = noticias.reverse().slice(0, qtd);
+
+  if(!quantidadeNoticias || quantidadeNoticias === 0) {
+    return alert('Insira quantas noticias deseja ver, sem ser zero.');
+  } else {
+    const qdtNoticias = ultimasNoticias.length;
+    let contador = 0;
+    ultimasNoticias.forEach((noticia) => {
+      paragrafo.innerText = `${qdtNoticias.length - contador}º) ${noticia}.`;
+    });
+  }
+
 }
 
-// limpa lista anterior
-const ul = document.getElementById('lista-noticias');
-ul.innerHTML = '';
-
-// lista todas as notícias na ordem em que foram inseridas
-noticias.forEach(noticia => {
-const li = document.createElement('li');
-li.textContent = noticia;
-ul.appendChild(li);
-});
+function mostrarNoticiasCadastradas(noticias) {
+  document.querySelector('#noticias-cadastradas').innerText = noticias.length;
 }
